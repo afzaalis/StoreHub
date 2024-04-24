5766939e367765c75e60adb9cbbdfbea6e22b394
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-04-24 10:06:53 +0800

    lupa

---
+++ Payment.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.storehub;
+
+/**
+ *
+ * @author aku
+ */
+public class Payment {
+    protected double accountBalance;
+
+    public Payment(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+}
